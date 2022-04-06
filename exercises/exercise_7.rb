b5e748368f0a1f9327399ff37e168c398a6203fb
Abhiram <abhiram.satha@gmail.com>
2022-04-06 21:14:51 +0000

    Completed exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,31 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+class Store < ActiveRecord::Base
+  validates :name, length: { minimum: 3,
+  too_short: "%{count} characters too short"
+  }
+  validates :annual_revenue, numericality: true
+  validates :annual_revenue, numericality: {
+    greater_than_or_equal_to: 0
+  }
+end
+
+
+
+class Employee < ActiveRecord::Base
+  validates :first_name, :last_name, presence: true
+  validates :hourly_rate, numericality: {
+    less_than_or_equal_to: 200
+  }
+  validates :hourly_rate, numericality: {
+    greater_than_or_equal_to: 40 
+  }
+  validates :store_id, presence: true
+end
+
+store_name = gets.chomp
+
+new_store = Store.create(name: "#{store_name}")
+puts "Error message #{new_store.errors.full_messages}"
